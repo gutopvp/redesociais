63a240a01196028daf42502e3b5b78a84675192e
gutopvp <108087931+gutopvp@users.noreply.github.com>
2024-10-25 09:25:08 -0300

    gilda

--- informacoesGlobais/informacoesGlobais.js
+++ informacoesGlobais/informacoesGlobais.js
@@ -1,10 +1,14 @@
 const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 async function visualizarInformacoesGlobais(){ 
-const res= await fetch(url)
+const res = await fetch(url)
 const dados = await res.json()
 console.log(dados);
 const paragrafo = document.createElement('P')
 paragrafo.classList.add('graficos-container_texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
+const container=documento.getElementById('graficos-container')
+container.appendChild(paragrafo)
 }
 visualizarInformacoesGlobais()
     
